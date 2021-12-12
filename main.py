75713eb1511800552c6745f89b2c094722ca8e4e
Naufal Anbial Falah <75089762+nafnaufal@users.noreply.github.com>
2021-12-12 22:07:00 +0700

    new main

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
-from data import *
-import requests as rq
+from process import *
+import requests as rq, os
 
 loop = True
 #main loop
 while loop:
+    os.system('cls')
 
     search = input('Enter game name: ')
     session = Data(search)
 
+    showListGame(session.game_name, session.game_price)
+
     select = input('Pilih nomor game :')
     sel_game = rq.get('https://www.cheapshark.com/api/1.0/games?id='+ str(session.game_id[int(select)-1])).json()
-
-    counter = 0
-    for i in sel_game['deals']:
-        print(counter + 1, '.', session.store_name[int(i['storeID'])-1])
-        print(sel_game['deals'][counter]['price'])
-        print(sel_game['deals'][counter]['retailPrice'])
-        print("Hemat = ", float(sel_game['deals'][counter]['retailPrice']) - float(sel_game['deals'][counter]['price']))
-        redic = "https://www.cheapshark.com/redirect?dealID="+sel_game['deals'][counter]['dealID']
-        print(redic)
-        counter += 1
     
-    del session
+
+    showStore(sel_game, session.store_name, session.game_name[int(select)-1])
 
     ulang = input('Ulang?(Y/N)')
-    if ulang == 'Y':
+    if ulang == 'Y' or ulang == 'y':
         loop = True
-    elif ulang == 'N':
+        session.delete()
+    elif ulang == 'N' or ulang == 'n':
         loop = False
     else:
         loop = False
