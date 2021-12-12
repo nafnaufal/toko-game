4ef4cad6f94abf0647b871cf11cc45f20ce46ff9
Naufal Anbial Falah <75089762+nafnaufal@users.noreply.github.com>
2021-12-12 20:22:58 +0700

    update

--- main.py
+++ main.py
@@ -1,39 +1,33 @@
-import requests
-import webbrowser
-import numpy as np
-import pandas as pd 
-pd.set_option('display.max_colwidth', -1)
-s = input()
-s = s.replace(' ','_')
-df = pd.read_json('https://www.cheapshark.com/api/1.0/games?title='+s)
-df['usd'] = df['cheapest']
-df['idr'] = df['cheapest']*14355
+from data import *
+import requests as rq
 
-# print(df[['external','usd','idr']])
+loop = True
+#main loop
+while loop:
 
-# a = int(input())
-# print(df.iloc[a,4])
-# d = (df.iloc[a,3])
-# b = 'https://www.cheapshark.com/redirect?dealID='+d
-# webbrowser.open(b, new=1) # link yg akan dituju
+    search = input('Enter game name: ')
+    session = Data(search)
 
-# cek = df.iloc[1,0]
-# cek = cek(str)
-# print(type(cek))
-# df['gameID'] = df['gameID'].astype(str)
-# cek = df.iloc[1,0]
-# cek = str(cek)
-# print(cek)
-aa = '233214'
+    select = input('Pilih nomor game :')
+    sel_game = rq.get('https://www.cheapshark.com/api/1.0/games?id='+ str(session.game_id[int(select)-1])).json()
 
-cf = pd.read_json('https://www.cheapshark.com/api/1.0/games?id='+aa)
+    counter = 0
+    for i in sel_game['deals']:
+        print(counter + 1, '.', session.store_name[int(i['storeID'])-1])
+        print(sel_game['deals'][counter]['price'])
+        print(sel_game['deals'][counter]['retailPrice'])
+        print("Hemat = ", float(sel_game['deals'][counter]['retailPrice']) - float(sel_game['deals'][counter]['price']))
+        redic = "https://www.cheapshark.com/redirect?dealID="+sel_game['deals'][counter]['dealID']
+        print(redic)
+        counter += 1
+    
+    del session
 
-print(cf)
-
-# cf = pd.read_json('https://www.cheapshark.com/api/1.0/games?id='+cek)
-# for game in df['gameID']:
-#     i = i + 1
-#     cek = df.iloc[i,0]
-#     cek = cek(str)
-#     print(type(cek))
-#     cf = pd.read_json('https://www.cheapshark.com/api/1.0/games?id='+df.iloc[i,0])
+    ulang = input('Ulang?(Y/N)')
+    if ulang == 'Y':
+        loop = True
+    elif ulang == 'N':
+        loop = False
+    else:
+        loop = False
+        print('Input salah')
